c4c7336a3cdd830695307abeb472b6186dd8dd96
gooodinho <andrey09132000@gmail.com>
2022-07-26 19:58:29 +0300

    test user creation form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+# TEST USER CREATION
+def user_register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        user = form.save(commit=False)
+        print("New user username:", user)
+    context = {'form': form}
+    return render(request, 'users/register.html', context=context)
