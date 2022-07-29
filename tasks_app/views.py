72d27f7145fe473ed11a9d96d84bdade0d915223
gooodinho <andrey09132000@gmail.com>
2022-07-29 16:56:32 +0300

    check if user is authenticated

--- tasks_app/views.py
+++ tasks_app/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='login')
+
 def output_all_tasks(request):
-    username = request.user.username
-    user = User.objects.get(username=username)
-    tasks = user.task_set.all()
+    tasks = []
+    if request.user.is_authenticated:
+        username = request.user.username
+        user = User.objects.get(username=username)
+        tasks = user.task_set.all()
     context = {'tasks': tasks}
     return render(request, 'tasks_app/tasks.html', context)
+
+
+@login_required(login_url='login')
+def add_task(request):
+    pass
