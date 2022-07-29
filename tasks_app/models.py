2a54eefca94313d30f81414f0cbe13dce8906536
gooodinho <andrey09132000@gmail.com>
2022-07-29 16:32:17 +0300

    update Task model

--- tasks_app/models.py
+++ tasks_app/models.py
@@ -1,7 +1,11 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
     task_name = models.CharField('task\'s name', max_length=1000)
+    status = models.BooleanField('task\'s status(done?)', default=False, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "task"
