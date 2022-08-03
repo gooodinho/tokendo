909428aad7deb8c1685bd3b4a7709295e626e7e6
gooodinho <andrey09132000@gmail.com>
2022-08-03 13:27:49 +0300

    update Profile model

--- users/models.py
+++ users/models.py
@@ -2,8 +2,10 @@ import uuid
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=300, blank=True, null=True)
     username = models.CharField(max_length=300, blank=True, null=True)
