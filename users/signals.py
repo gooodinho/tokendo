6c97b584a9a44eb97e0d6fdac862bad242492144
gooodinho <andrey09132000@gmail.com>
2022-08-03 22:24:43 +0300

    update user signals

--- users/signals.py
+++ users/signals.py
@@ -1,13 +1,16 @@
-from .service import create_profile, update_profile
+from .service import create_profile, get_profile_by_user, update_profile
 
 
-def create_user_profile_signal(sender, instance, created, **kwargs):
+def create_user_profile_signal(sender, instance, created, **kwargs) -> None:
     if created is True:
         user = instance
         create_profile(user, user.username)
 
 
-def update_user_profile_signal(sender, instance, created, **kwargs):
+def update_user_profile_signal(sender, instance, created, **kwargs) -> None:
     if created is not True:
         user = instance
-        update_profile(user, user.username, user.first_name, user.email)
+        if get_profile_by_user(user) is not None:
+            update_profile(user, user.username, user.first_name, user.email)
+        else:
+            create_profile(user, user.username)
