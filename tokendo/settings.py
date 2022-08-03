881ccd7120a5872797fc6c3e7ecd2c6f9cbe2627
gooodinho <andrey09132000@gmail.com>
2022-08-03 13:23:05 +0300

    create Profile model

--- tokendo/settings.py
+++ tokendo/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=300, blank=True, null=True)
+    username = models.CharField(max_length=300, blank=True, null=True)
+    email = models.EmailField(max_length=300, blank=True, null=True)
+    profile_image = models.ImageField(upload_to="profiles/", default="profiles/default.png", blank=True, null=True)
+    created = models.DateTimeField(default=timezone.now)
