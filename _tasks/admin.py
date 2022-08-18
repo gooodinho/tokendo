900c8eb0b776754b23da0782aeb0abdb94165cbd
gooodinho <andrey09132000@gmail.com>
2022-08-18 20:53:01 +0300

    update admin panel registration

--- _tasks/admin.py
+++ _tasks/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
+from django import forms
+
 from .models import Task, Project
 
 
-admin.site.register(Task)
-admin.site.register(Project)
+class TaskAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'initial' not in kwargs:
+            try:
+                self.fields['project'].queryset = Project.objects.filter(owner=self.instance.owner)
+            except:
+                pass
+        else:
+            self.fields['project'].disabled = True
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    form = TaskAdminForm
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('owner', 'created')
+        else:
+            return ('created',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('owner', 'created')
+        else:
+            return ('created',)
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('user', 'created')
+        else:
+            return ('created',)
